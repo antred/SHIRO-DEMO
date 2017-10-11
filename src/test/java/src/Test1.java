efaaa33d1c94aa1bac3a75cf0c117a1697a71617
Xulf <zxcmy1988@163.com>
2017-10-11 12:04:53 +0800

    自定义realm实现和测试

--- src/test/java/src/Test1.java
+++ src/test/java/src/Test1.java
@@ -13,11 +13,47 @@ import org.junit.Test;
 
 public class Test1 {
 	
+	/**
+	 * shiro登陆测试
+	 */
 	@Test
-	public void testHelloWorld() {
+	public void testShiroLogin() {
 		
 		// 1.使用Shiro.ini文件初始化SecurityManagerg工厂
 		Factory<SecurityManager>factoty = new IniSecurityManagerFactory("classpath:shiro.ini");
+		
+		// 2.获取SecurityManager实例并绑定到SecurityUtils
+		SecurityManager securityManager = factoty.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		// 3.获取Subject对象并创建用户名密码Token
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang","123");
+		
+		try {
+			// 4.用户登录
+			subject.login(token);
+		} catch (Exception e) {
+			// 5.身份验证失败
+			//e.printStackTrace();
+		}
+		
+		// 断言用户是否登陆
+		Assert.assertEquals(true, subject.isAuthenticated());
+		
+		// 6.用户推出
+		subject.logout();
+		
+	}
+	
+	/**
+	 * 自定义Realm测试
+	 */
+	@Test
+	public void testShiroRealm() {
+		
+		// 1.使用Shiro.ini文件初始化SecurityManagerg工厂
+		Factory<SecurityManager>factoty = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
 		
 		// 2.获取SecurityManager实例并绑定到SecurityUtils
 		SecurityManager securityManager = factoty.getInstance();
